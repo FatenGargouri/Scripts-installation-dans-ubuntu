cb99c5288e4e8f1abdcc6932b4461d71bdd7b28f
FatenGargouri <85526389+FatenGargouri@users.noreply.github.com>
2023-10-22 23:30:52 +0200

    Update install_kubernetes.sh

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl 
+# Vérifiez si Docker est installé, car Minikube dépend de Docker.
+if ! command -v docker &> /dev/null; then
+  echo "Docker n'est pas installé. Veuillez installer Docker avant de continuer."
+  exit 1
+fi
 
-swapoff -a  <<<<<<<< just disable it in /etc/fstab instead
-
-
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list"
-
-apt-get update
+sudo usermod -aG docker faten
 
 
-sudo -s
+# Télécharge Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
 
-apt-get install -y kubelet=1.26.1-00 kubeadm=1.26.1-00 kubectl=1.26.1-00
-apt-mark hold kubelet kubeadm kubectl
-
-# check swap config, ensure swap is 0
-free -m
-
-
-### ONLY ON CONTROL NODE .. control plane install:
-kubeadm init --pod-network-cidr 10.10.0.0/16 --kubernetes-version 1.26.1 --node-name k8s-control
+# Télécharge kubectl
+curl -Lo kubectl https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
 
-# add Calico 3.25 CNI 
-### https://docs.tigera.io/calico/3.25/getting-started/kubernetes/quickstart
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
-wget https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml
-vi custom-resources.yaml <<<<<< edit the CIDR for pods if its custom
-kubectl apply -f custom-resources.yaml
+# Start Minikube with the Docker driver
+minikube start --vm-driver=docker
 
-# get worker node commands to run to join additional nodes into cluster
-kubeadm token create --print-join-command
+# Vérifiez que Minikube est correctement installé
+minikube status
 
-
-## ONLY ON WORKER nodes
-Run the command from the token create output above... this script for installing kubernetes successfully ?
